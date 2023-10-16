1d2e1fc8d2a0a49f294d1911a4d63d19da6dfe8d
v3n4i <ven4i@icloud.com>
2023-10-16 21:06:26 +0400

    need commit last task

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,7 @@
+
+import java.util.Scanner;
 public class Application {
+    String[] Animal;
     public static void main(String[] args) {
 
         Animal h1 = new Animal("Бобик", 15, 15, "черный");
@@ -10,5 +13,15 @@ public class Application {
         Duck duck1 = new Duck("Утка",1, 1, "белый");
         duck1.Fly();
         duck1.Say();
+        System.out.println("Введите команду из списка add/list/exit");
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        if (str.equals("add"))
 
-    }}
+        {Commands h2 = Commands.add;
+            switch (h2){
+                case add:
+                    System.out.println("Добавьте животное");
+                    break;}
+        }  }
+}
